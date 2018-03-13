af4580c949083e9d4e9eb4f1db7caa417e811c83
Robert D Gauthier <rgauthie@gmail.com>
2018-03-13 19:36:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,11 +17,11 @@ function destructivelyRemoveFirstKitten(){
 }
 
 function appendKitten(name){
-  kittens.push(name)
+  return [...kittens, name]
 }
 
 function prependKitten(name){
-  kittens.unshift(name)
+  return [name, ...kittens]
 }
 
 function removeLastKitten(){
